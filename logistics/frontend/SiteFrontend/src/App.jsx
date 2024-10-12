2eef1e89eb1364e300d6cfed1e3c6bf488fe43ec
cosmascodes <75983101+Gacalow@users.noreply.github.com>
2024-10-12 18:14:30 +0300

    Push that

--- logistics/frontend/SiteFrontend/src/App.jsx
+++ logistics/frontend/SiteFrontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./middleware";
 import Login from "./pages/@auth/login";
 import Signup from "./pages/@auth/signup";
+import Dashboard from "./pages/dashboard/page";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
 
         {/* Protect these routes */}
         {/* <Route
