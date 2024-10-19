6dd963d2d6f4a81bf2a4e07c6de203232ad11216
cosmascodes <56020081+cosmascodes@users.noreply.github.com>
2024-10-19 13:49:59 +0300

    Push the header

--- logistics/frontend/SiteFrontend/src/pages/dashboard/Header.jsx
+++ logistics/frontend/SiteFrontend/src/pages/dashboard/Header.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { FaPlus } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div className="flex justify-between items-center p-4">
-      <input
-        type="text"
-        placeholder="Search here"
-        className="bg-gray-600 text-white rounded-full px-4 py-2 focus:outline-none w-1/3"
-      />
+    <div className="flex justify-between items-center p-4 px-10">
+      <div className="relative w-80">
+        <input
+          type="text"
+          placeholder="Search here"
+          className="bg-gray-600 text-white rounded-lg px-4 py-2 pl-10 focus:outline-none w-full"
+        />
+        <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
+          <FaSearch className="h-5 w-5" />
+        </div>
+      </div>
       <div className="flex items-center">
         <button className="bg-blue-500 text-white rounded-lg px-4 py-2 flex items-center">
           <FaPlus className="mr-2" />
