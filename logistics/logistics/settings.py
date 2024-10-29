ffaac3228a51d9f7648670af374c773637dc8ff5
DevFreezy <64470491+franfreezy@users.noreply.github.com>
2024-10-29 12:58:13 +0300

    Update settings.py

--- logistics/logistics/settings.py
+++ logistics/logistics/settings.py
@@ -83,7 +83,7 @@ SIMPLE_JWT = {
 
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -92,6 +92,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'logistics.urls'
